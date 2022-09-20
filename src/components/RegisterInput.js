6f35acb86a73c54abe9eaaadf9b4e3c679b5c239
Arie <arie@gmail.com>
2022-09-20 17:46:12 +0700

    add register logic

--- src/components/RegisterInput.js
+++ src/components/RegisterInput.js
@@ -1,12 +1,39 @@
-const RegisterInput = () => {
+import { useState } from "react"
+import useInput from "../contexts/useInput"
+
+const RegisterInput = ({register}) => {
+    const [name,handleNameChange] = useInput('')
+    const [email,handleEmailChange] = useInput('')
+    const [password,handlePasswordChange] = useInput('')
+    const [cnfrmPassword,handleCnfrmPasswordChange] = useInput('')
+    const [errors,setErrors] = useState({})
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        
+        let listError = {}
+        listError = {}
+
+        if (password !== cnfrmPassword) {
+            listError['cnfrmpassword'] = "password does not match"
+        }
+
+        setErrors(listError)
+
+        if (listError == {}) {
+            register({name,email,password})
+        }
+    }
+
     return (
-        <form className="form-input">
-        <input type="text" placeholder="name" required />
-        <input type="email" placeholder="email" required />
-        <input type="password" placeholder="password" required />
-        <input type="password" placeholder="confirm password" required />
-        <button className="btn-auth">Sign Up</button>
-    </form>
+        <form onSubmit={onSubmitHandler} className="form-input">
+            <input type="text" value={name} onChange={handleNameChange} placeholder="name" required />
+            <input type="email" value={email} onChange={handleEmailChange} placeholder="email" required />
+            <input type="password" value={password} onChange={handlePasswordChange} placeholder="password" required />
+            <input type="password" value={cnfrmPassword} onChange={handleCnfrmPasswordChange} placeholder="confirm password" required />
+            <p>{errors['cnfrmpassword']}</p>
+            <button className="btn-auth">Sign Up</button>
+        </form>
     )
 }
 
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link , useNavigate} from "react-router-dom"
 import RegisterInput from "../components/RegisterInput"
+import { register } from "../utils/network-data";
 
 const RegisterPage = () => {
+    const navigate = useNavigate();
+
+    const onRegisterHandler = async (user) => {
+        const {error} = await register(user)
+        if (!error) {
+            navigate('/')
+        }
+    }
+
     return (
         <div className="container-form">
             <div className="form-parent">
                 <h2>Sign Up!</h2>
-                <RegisterInput />
+                <RegisterInput register={onRegisterHandler} />
                 <p><Link to='/login'>Have account?</Link></p>
             </div>
         </div>
